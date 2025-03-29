5bbc1d7c8a3bd855b4df5f81309c9cb551fcfd21
adri <129704623+adrianamil@users.noreply.github.com>
2025-03-28 22:50:22 -0500

    Update CaesarShiftCipherTest.java
    FEAT: Metodos de prueba

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,44 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CaesarShiftCipherTest {
 
+    private final CaesarShiftCipher cipher = new CaesarShiftCipher();
+
+    @Test
+    public void testPositiveShift() {
+        assertEquals("def", cipher.CaesarShiftCipher("abc", 3));
+    }
+
+    @Test
+    public void testNegativeShift() {
+        assertEquals("abc", cipher.CaesarShiftCipher("def", -3));
+    }
+
+    @Test
+    public void testShiftWithWrapAround() {
+        assertEquals("abc", cipher.CaesarShiftCipher("xyz", 3));
+    }
+
+    @Test
+    public void testSpaceCharacter() {
+        assertEquals("a b c", cipher.CaesarShiftCipher("x y z", 3));
+    }
+
+    @Test
+    public void testInvalidInput() {
+        assertEquals("invalid", cipher.CaesarShiftCipher("abc123", 3));
+    }
+
+    @Test
+    public void testEmptyMessage() {
+        assertEquals("", cipher.CaesarShiftCipher("", 3));
+    }
+
+    @Test
+    public void testLargeShiftValue() {
+        assertEquals("def", cipher.CaesarShiftCipher("abc", 29));  // 29 % 26 = 3
+    }
 }
